aaee65f4537b8ef4059be7d1ae244f9f4e875f48
Reyhaneh <reyhaneh.gh.davodi@gmail.com>
2023-09-03 23:18:30 +0330

    send an email with news title and description

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import smtplib
 import ssl
+
 def send_email(cont):
      port=465
      host="smtp.gmail.com"
@@ -12,20 +13,18 @@ def send_email(cont):
           server.sendmail(username,username,cont)
 
 
-
-
-
 api="24bc313188794f928cf0d6e7ef838c38"
-url=("https://newsapi.org/v2/everything?"
-     "q=tesla&from=2023-08-02&sortBy=publishedAt&api"
-     "Key=24bc313188794f928cf0d6e7ef838c38")
+url=("https://newsapi.org/v2/everything?q=tesla&from=2023-08-03&sort"
+     "By=publishedAt&apiKey=24bc313188794f928cf0d6e7ef838c38")
 # Make a Requests
 request=requests.get(url)
 # get a dictionary with data
 content=request.json()
+print(content)
 # access the articles description and titles
+news=""
 for article in content['articles']:
-     news=article['title']+ "\n" + article['description']
-     send_email(news)
-     # print(article['title'])
-     # print(article['description'])
+     news=news+article['title']+ "\n" + article['description']+2*"\n"
+news=news.encode("utf-8")
+     # send the data as an email
+send_email(news)
