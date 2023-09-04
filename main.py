bf34b03e201c146724e2c9630074bad570004c55
Reyhaneh <reyhaneh.gh.davodi@gmail.com>
2023-09-04 21:00:54 +0330

    add link ,shows jus english news,add subject to the email,environment password,

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 import smtplib
 import ssl
-
+import os
 def send_email(cont):
      port=465
      host="smtp.gmail.com"
      username="reyhaneh.gh.davodi@gmail.com"
-     password="rsorsftxzuzvvhir"
+     password=os.getenv("NEWSPASSWORD")
      context=ssl.create_default_context()
      with smtplib.SMTP_SSL(host,port,context=context) as server:
           server.login(username,password)
@@ -15,7 +15,7 @@ def send_email(cont):
 
 api="24bc313188794f928cf0d6e7ef838c38"
 url=("https://newsapi.org/v2/everything?q=tesla&from=2023-08-03&sort"
-     "By=publishedAt&apiKey=24bc313188794f928cf0d6e7ef838c38")
+     "By=publishedAt&apiKey=24bc313188794f928cf0d6e7ef838c38&language=en")
 # Make a Requests
 request=requests.get(url)
 # get a dictionary with data
@@ -23,8 +23,12 @@ content=request.json()
 print(content)
 # access the articles description and titles
 news=""
-for article in content['articles']:
-     news=news+article['title']+ "\n" + article['description']+2*"\n"
+for article in content['articles'][:20]:
+     if article['title'] is not None:
+         news=("Subject: Today's news "+"\n"+
+                 news+article['title']+ "\n"
+               + article['description']
+               +'\n'+article['url']+ 2*"\n")
 news=news.encode("utf-8")
      # send the data as an email
 send_email(news)
